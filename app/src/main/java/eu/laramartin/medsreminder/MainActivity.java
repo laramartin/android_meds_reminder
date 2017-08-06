9d5339361d9bef611952714fa3958e62e46fb498
Lara Martin <larikimc@gmail.com>
2017-08-06 15:30:43 +0200

    load first fragment

--- app/src/main/java/eu/laramartin/medsreminder/MainActivity.java
+++ app/src/main/java/eu/laramartin/medsreminder/MainActivity.java
@@ -1,7 +1,8 @@
 package eu.laramartin.medsreminder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.fragment_container, new MedsFragment()).commit();
     }
 }
---
+++ app/src/main/java/eu/laramartin/medsreminder/MedsFragment.java
@@ -0,0 +1,9 @@
+package eu.laramartin.medsreminder;
+
+import android.support.v4.app.Fragment;
+
+
+public class MedsFragment extends Fragment {
+
+
+}

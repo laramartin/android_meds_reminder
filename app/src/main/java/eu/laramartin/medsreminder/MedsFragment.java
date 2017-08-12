ed314ca701c7f833b2264e87ce0c20c49d7871cb
Lara Martin <lara.martin.carretero@gmail.com>
2017-08-12 14:47:58 +0200

    add logout menu option on actionbar

--- app/src/main/java/eu/laramartin/medsreminder/MedsFragment.java
+++ app/src/main/java/eu/laramartin/medsreminder/MedsFragment.java
@@ -8,10 +8,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 
 public class MedsFragment extends Fragment {
@@ -23,12 +29,38 @@ public class MedsFragment extends Fragment {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
+    Unbinder unbinder;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_meds, container, false);
+        unbinder = ButterKnife.bind(this, view);
         Log.i(LOG_TAG, "meds fragment selected");
         AppCompatActivity appCompatActivity = ((AppCompatActivity) getActivity());
         appCompatActivity.setSupportActionBar(toolbar);
-        return inflater.inflate(R.layout.fragment_meds, container, false);
+        setHasOptionsMenu(true);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.action_bar_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_logout:
+                Toast.makeText(getContext(), "logout", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

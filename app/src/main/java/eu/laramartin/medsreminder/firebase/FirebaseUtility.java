696228bbcedf293f9709eafe29ac254952dfea84
Lara Martin <lara.martin.carretero@gmail.com>
2017-08-18 19:57:45 +0200

    write onto DB a user object

--- app/src/main/java/eu/laramartin/medsreminder/firebase/FirebaseUtility.java
+++ app/src/main/java/eu/laramartin/medsreminder/firebase/FirebaseUtility.java
@@ -8,11 +8,19 @@ import android.support.v4.app.FragmentActivity;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
 
 import eu.laramartin.medsreminder.MainActivity;
 import eu.laramartin.medsreminder.R;
+import eu.laramartin.medsreminder.model.User;
 
 import static android.support.v4.content.ContextCompat.startActivity;
 
@@ -41,5 +49,35 @@ public class FirebaseUtility {
                         fragmentActivity.finish();
                     }
                 });
+    }
+
+    public static void createUserIfDoesntExist() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = database.getReference("user");
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = auth.getCurrentUser();
+        if (firebaseUser!= null) {
+            DatabaseReference userReference = databaseReference.child(firebaseUser.getUid());
+            userReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+//            if (userReference. )
+
+
+            User user = new User();
+            user.setEmail(firebaseUser.getEmail());
+            user.setId(firebaseUser.getUid());
+            databaseReference.child(firebaseUser.getUid())
+                    .setValue(user);
+
+        }
     }
 }

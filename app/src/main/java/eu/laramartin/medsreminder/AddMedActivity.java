340bc539f49bf18eae28be596e27cf60bd8eba9e
Lara Martin <lara.martin.carretero@gmail.com>
2017-08-19 18:29:37 +0200

    add action bar and menu to add med screen

--- app/src/main/java/eu/laramartin/medsreminder/AddMedActivity.java
+++ app/src/main/java/eu/laramartin/medsreminder/AddMedActivity.java
@@ -1,7 +1,13 @@
 package eu.laramartin.medsreminder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
 
 public class AddMedActivity extends AppCompatActivity {
 
@@ -9,5 +15,26 @@ public class AddMedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_med);
+        ButterKnife.bind(this);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_med_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_med:
+                // TODO: 19.08.17 Lara: implement saving medication to db
+                Toast.makeText(this, "Saving med to DB!", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+        return false;
     }
 }

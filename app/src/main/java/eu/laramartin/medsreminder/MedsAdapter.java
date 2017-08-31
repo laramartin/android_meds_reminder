daacca15aa1c099c1bcd7353126bf72de5ac4082
Lara Martin <lara.martin.carretero@gmail.com>
2017-08-31 20:34:57 +0200

    populate list item with data

--- app/src/main/java/eu/laramartin/medsreminder/MedsAdapter.java
+++ app/src/main/java/eu/laramartin/medsreminder/MedsAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -25,9 +26,18 @@ public class MedsAdapter extends RecyclerView.Adapter<MedsAdapter.MedViewHolder>
 
     public class MedViewHolder extends RecyclerView.ViewHolder {
 
-        // TODO: 20.08.17 Lara: remove this TextView from layout
-        @BindView(R.id.meds_text)
-        TextView medsText;
+        @BindView(R.id.med_icon)
+        ImageView medIcon;
+        @BindView(R.id.med_name)
+        TextView medName;
+        @BindView(R.id.med_time) TextView medTime;
+        @BindView(R.id.med_days) TextView medDays;
+        @BindView(R.id.med_notes) TextView medNotes;
+        // TODO: 31.08.17 Lara: show next date to take medicine
+        @BindView(R.id.med_date)
+        TextView medDate;
+        // TODO: 31.08.17 Lara: show current time status (on time, soon, it' time)
+        @BindView(R.id.med_time_status) TextView medTimeStatus;
 
         public MedViewHolder(View itemView) {
             super(itemView);
@@ -46,7 +56,19 @@ public class MedsAdapter extends RecyclerView.Adapter<MedsAdapter.MedViewHolder>
     @Override
     public void onBindViewHolder(MedViewHolder holder, int position) {
         Med med = meds.get(position);
-        holder.medsText.setText(med.toString());
+        holder.medName.setText(med.getName());
+        holder.medIcon.setImageResource(MedsUtility.getMedIcon(med.getDosage()));
+        // TODO: 31.08.17 Lara: handle set date
+        holder.medDate.setText("07/07/2017");
+        holder.medTime.setText(med.getTime());
+        // TODO: 31.08.17 Lara: format days like "Days: Mo, Tu"
+        holder.medDays.setText(med.getDays());
+        // TODO: 31.08.17 Lara: format notes like "Notes: blabla..." 
+        if (med.getNotes() != null && !med.getNotes().isEmpty()) {
+            holder.medNotes.setText(med.getNotes());
+        } else {
+            holder.medNotes.setVisibility(View.INVISIBLE);
+        }
     }
 
     @Override

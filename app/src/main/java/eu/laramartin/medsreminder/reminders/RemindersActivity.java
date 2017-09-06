a0d014385657b324b017691bb472a3b39e544643
Lara Martin <lara.martin.carretero@gmail.com>
2017-09-06 19:50:38 +0200

    Set adapter for RemindersActivity with dummy data

--- app/src/main/java/eu/laramartin/medsreminder/reminders/RemindersActivity.java
+++ app/src/main/java/eu/laramartin/medsreminder/reminders/RemindersActivity.java
@@ -1,15 +1,39 @@
 package eu.laramartin.medsreminder.reminders;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import eu.laramartin.medsreminder.R;
+import eu.laramartin.medsreminder.model.Med;
 
 public class RemindersActivity extends AppCompatActivity {
+
+    @BindView(R.id.reminder_recyclerview)
+    RecyclerView recyclerView;
+
+    private RemindersAdapter remindersAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
+        ButterKnife.bind(this);
+
+        remindersAdapter = new RemindersAdapter();
+        recyclerView.setAdapter(remindersAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        setAdapterData();
+    }
+
+    private void setAdapterData() {
+        // TODO: 06.09.17 Lara: read db
+
+        Med med = new Med();
+        med.setName("medication 1");
+        remindersAdapter.add(med);
     }
 }

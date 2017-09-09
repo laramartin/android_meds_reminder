f4256b8aaaf4fab23b64b775017f19ab6b04432d
Lara Martin <lara.martin.carretero@gmail.com>
2017-09-09 18:48:21 +0200

    calculate difference to next day to take medicine

--- app/src/main/java/eu/laramartin/medsreminder/reminders/RemindersUtility.java
+++ app/src/main/java/eu/laramartin/medsreminder/reminders/RemindersUtility.java
@@ -9,7 +9,9 @@ import com.firebase.jobdispatcher.Job;
 import com.firebase.jobdispatcher.Lifetime;
 import com.firebase.jobdispatcher.Trigger;
 
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import eu.laramartin.medsreminder.model.Med;
 
@@ -24,7 +26,23 @@ public class RemindersUtility {
         if (isInitialized) {
             return;
         }
-        int REMINDER_INTERVAL_SECONDS = (int) getTimeInSeconds(med);
+        int REMINDER_INTERVAL_SECONDS = 0;
+
+        // days of the week when med has to be taken
+        // "MoTu" would be [2, 3]
+        List<Integer> calendarDays =  getMedDays(med);
+        String[] timeSplitted = getMedTimeSplit(med);
+        int hours = Integer.valueOf(timeSplitted[0]);
+        int minutes = Integer.valueOf(timeSplitted[1]);
+
+        // TODO: 09.09.17 Lara: for each day to take med, calculate day and hours difference in seconds
+        // TODO: 09.09.17 Lara: and schedule a new job/alarm
+        for (Integer calendarDay: calendarDays) {
+            long diffInSeconds = timeToNextGivenDay(calendarDay, hours, minutes) / 1000;
+
+        }
+
+        // TODO: 09.09.17 Lara: create an alarm and move to inside for loop
         Driver driver = new GooglePlayDriver(context);
         FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(driver);
         Job job = dispatcher.newJobBuilder()
@@ -49,13 +67,64 @@ public class RemindersUtility {
         isInitialized = true;
     }
 
-    private static long getTimeInSeconds(Med med) {
+    private static List<Integer> getMedDays(Med med) {
+        String medDays = med.getDays();
+        List<Integer> weekDays = getDaysOfWeek(medDays);
+        return weekDays;
+    }
+
+    private static List<Integer> getDaysOfWeek(String medDays) {
+        List<Integer> weekDays = new ArrayList<>();
+        if (medDays.contains("Mo")) {
+            weekDays.add(Calendar.MONDAY);
+        }
+        if (medDays.contains("Tu")) {
+            weekDays.add(Calendar.TUESDAY);
+        }
+        if (medDays.contains("We")) {
+            weekDays.add(Calendar.WEDNESDAY);
+        }
+        if (medDays.contains("Th")) {
+            weekDays.add(Calendar.THURSDAY);
+        }
+        if (medDays.contains("Fr")) {
+            weekDays.add(Calendar.FRIDAY);
+        }
+        if (medDays.contains("Sa")) {
+            weekDays.add(Calendar.SATURDAY);
+        }
+        if (medDays.contains("Su")) {
+            weekDays.add(Calendar.SUNDAY);
+        }
+        return weekDays;
+    }
+
+    private static String[] getMedTimeSplit(Med med) {
         String time = med.getTime();
         String[] timeParts = time.split(":");
-        int hours = Integer.valueOf(timeParts[0]);
-        int minutes = Integer.valueOf(timeParts[1]);
-        long hoursInSeconds = TimeUnit.HOURS.toSeconds(hours);
-        long minutesInSeconds = TimeUnit.MINUTES.toSeconds(minutes);
-        return (hoursInSeconds + minutesInSeconds);
+        return timeParts;
     }
+
+    public static long timeToNextGivenDay(int targetDoW, int targetHoW, int targetMoW) {
+        Calendar now = Calendar.getInstance();
+        Calendar next = Calendar.getInstance();
+        int nowDoW = now.get(Calendar.DAY_OF_WEEK);
+        int addDays = (nowDoW + targetDoW) % 7;
+        next.add(Calendar.DAY_OF_YEAR, addDays);
+        next.set(Calendar.HOUR_OF_DAY, targetHoW);
+        next.set(Calendar.MINUTE, targetMoW);
+
+        long date = next.getTimeInMillis();
+        long diff = date - now.getTimeInMillis();
+
+        // diff is in the past
+        if (diff < 0) {
+            next.add(Calendar.DAY_OF_YEAR, 7);
+            date = next.getTimeInMillis();
+            diff = date - now.getTimeInMillis();
+        }
+
+        return diff;
+    }
+
 }

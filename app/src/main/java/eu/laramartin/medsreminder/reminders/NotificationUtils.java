641c15ee112fc34baf11c2389736c6bfafee481c
Lara Martin <lara.martin.carretero@gmail.com>
2017-09-10 23:01:56 +0200

    code clean up

--- app/src/main/java/eu/laramartin/medsreminder/reminders/NotificationUtils.java
+++ app/src/main/java/eu/laramartin/medsreminder/reminders/NotificationUtils.java
@@ -17,24 +17,9 @@ import eu.laramartin.medsreminder.R;
 
 class NotificationUtils {
 
-    /*
-     * This notification ID can be used to access our notification after we've displayed it. This
-     * can be handy when we need to cancel the notification, or perhaps update it. This number is
-     * arbitrary and can be set to whatever you like. 1138 is in no way significant.
-     */
     private static final int MED_REMINDER_NOTIFICATION_ID = 857;
-    /**
-     * This pending intent id is used to uniquely reference the pending intent
-     */
     private static final int MED_REMINDER_PENDING_INTENT_ID = 566;
     private static final int ACTION_TAKE_MED_PENDING_INTENT_ID = 1;
-//    private static final int ACTION_IGNORE_PENDING_INTENT_ID = 14;
-
-//    public static void clearAllNotifications(Context context) {
-//        NotificationManager notificationManager = (NotificationManager)
-//                context.getSystemService(Context.NOTIFICATION_SERVICE);
-//        notificationManager.cancelAll();
-//    }
 
     public static void remindUserToTakeMed(Context context) {
 
@@ -42,14 +27,13 @@ class NotificationUtils {
                 .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
                 .setSmallIcon(R.drawable.ic_capsule_128)
                 .setLargeIcon(largeIcon(context))
-                .setContentTitle(context.getString(R.string.its_time_reminder_title))
-                .setContentText(context.getString(R.string.med_reminder_notification_text))
+                .setContentTitle(context.getString(R.string.reminder_notification_title))
+                .setContentText(context.getString(R.string.reminder_notification_body))
                 .setStyle(new NotificationCompat.BigTextStyle().bigText(
-                        context.getString(R.string.med_reminder_notification_text)))
+                        context.getString(R.string.reminder_notification_body)))
                 .setDefaults(Notification.DEFAULT_VIBRATE)
                 .setContentIntent(contentIntent(context))
                 .addAction(takeMedAction(context))
-//                .addAction(ignoreReminderAction(context))
                 .setAutoCancel(true);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
@@ -59,24 +43,10 @@ class NotificationUtils {
         NotificationManager notificationManager = (NotificationManager)
                 context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        /* WATER_REMINDER_NOTIFICATION_ID allows you to update or cancel the notification later on */
         notificationManager.notify(MED_REMINDER_NOTIFICATION_ID, notificationBuilder.build());
     }
 
-//    private static NotificationCompat.Action ignoreReminderAction(Context context) {
-//        Intent ignoreReminderIntent = new Intent(context, ReminderIntentService.class);
-//        ignoreReminderIntent.setAction(ReminderTasks.ACTION_DISMISS_NOTIFICATION);
-//        PendingIntent ignoreReminderPendingIntent = PendingIntent.getService(
-//                context,
-//                ACTION_IGNORE_PENDING_INTENT_ID,
-//                ignoreReminderIntent,
-//                PendingIntent.FLAG_UPDATE_CURRENT);
-//        NotificationCompat.Action ignoreReminderAction = new NotificationCompat.Action(R.drawable.ic_cancel_black_24px,
-//                "No, thanks.",
-//                ignoreReminderPendingIntent);
-//        return ignoreReminderAction;
-//    }
-
+    // TODO: 10.09.17 Lara: implement action adding a report to Firebase or remove completely
     static NotificationCompat.Action takeMedAction(Context context) {
         Intent takeMedIntent = new Intent(context, ReminderIntentService.class);
         PendingIntent takeMedPendingIntent = PendingIntent.getService(

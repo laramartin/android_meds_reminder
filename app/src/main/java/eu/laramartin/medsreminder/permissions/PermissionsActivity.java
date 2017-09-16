e52cbc9cba6cf440598d9103fc7f886fa6e71a1d
Lara Martin <lara.martin.carretero@gmail.com>
2017-09-16 18:23:12 +0200

    Implemented dialog to invite friend

--- app/src/main/java/eu/laramartin/medsreminder/permissions/PermissionsActivity.java
+++ app/src/main/java/eu/laramartin/medsreminder/permissions/PermissionsActivity.java
@@ -1,15 +1,31 @@
 package eu.laramartin.medsreminder.permissions;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import eu.laramartin.medsreminder.R;
+import eu.laramartin.medsreminder.common.DialogsUtility;
 
-public class PermissionsActivity extends AppCompatActivity {
+public class PermissionsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.fab_permissions)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permissions);
+        ButterKnife.bind(this);
+
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        DialogsUtility.showInviteFriendDialog(this);
     }
 }

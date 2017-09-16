cdb55927dbcd3f75f7e02fcb35c7e38b4a654517
Lara Martin <lara.martin.carretero@gmail.com>
2017-09-16 17:28:23 +0200

    create permissions Activity and base layout

---
+++ app/src/main/java/eu/laramartin/medsreminder/permissions/PermissionsActivity.java
@@ -0,0 +1,15 @@
+package eu.laramartin.medsreminder.permissions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import eu.laramartin.medsreminder.R;
+
+public class PermissionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permissions);
+    }
+}
